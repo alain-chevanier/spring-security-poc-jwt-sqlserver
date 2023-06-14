7aeae20af4261059f444860580e25faae17f2e50
Alaín Pérez <alain.chevanier@ciencias.unam.mx>
2023-06-14 17:26:57 -0600

    agregando la configuración para habilitar la auditabilidad de las entidades de jpa

--- src/main/java/com/wizeline/spring/security/auth/SpringBootSecuritySQLServerApplication.java
+++ src/main/java/com/wizeline/spring/security/auth/SpringBootSecuritySQLServerApplication.java
@@ -3,9 +3,11 @@ package com.wizeline.spring.security.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringBootSecuritySQLServerApplication {
 
 	public static void main(String[] args) {
